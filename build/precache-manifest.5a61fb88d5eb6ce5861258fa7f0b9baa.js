f6d23c6db3bdccf5872d83e61f314313d54de099
Oliver Lang <lang.oliver@gmail.com>
2020-05-22 14:40:38 +0200

    uibuild

---
+++ build/precache-manifest.5a61fb88d5eb6ce5861258fa7f0b9baa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ea080d9f8de111ecced42e97ed8442",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8bbfc628fa6f9abc16b",
+    "url": "/static/css/2.9adad3af.chunk.css"
+  },
+  {
+    "revision": "e6aa260fe4fd1982a377",
+    "url": "/static/css/main.067905d3.chunk.css"
+  },
+  {
+    "revision": "e8bbfc628fa6f9abc16b",
+    "url": "/static/js/2.c4f295cf.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.c4f295cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6aa260fe4fd1982a377",
+    "url": "/static/js/main.4a20077e.chunk.js"
+  },
+  {
+    "revision": "37e42bc47805ba865915",
+    "url": "/static/js/runtime-main.fac6ad05.js"
+  }
+]);
