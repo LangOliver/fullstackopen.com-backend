b09a3e11a11a28ba6f863e8d2fcdab011ad90bca
Oliver Lang <lang.oliver@gmail.com>
2020-05-23 14:46:24 +0200

    3.15: Phonebook database, step3+4

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ require('dotenv').config()
 const Person = require('./models/person')
 
 app.use(cors())
-app.use(express.json())
 app.use(express.static('build'))
+app.use(express.json())
 
 let persons = [
           {
@@ -51,6 +51,7 @@ let persons = [
           }
       
 ]
+
 
 
 var morgan = require('morgan')
@@ -126,7 +127,7 @@ app.post('/api/persons', (request, response) => {
   // response.json(person)
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   console.log("Find by id: ", request.params.id)
   Person.findById(request.params.id).then(person => {
     if (person) {
@@ -135,15 +136,37 @@ app.get('/api/persons/:id', (request, response) => {
       response.status(404).end()
     }
   })
+  .catch(error => {
+    next(error)
+  })
   
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+  Person.findByIdAndRemove(request.params.id).then(result=> {
+    response.status(204).end()
+  })
+ .catch(error => next(error))
 })
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
